88d047fdc1d51cc2564e3dfa9898ff751c6843fa
Bar Kotlyarov <Kotliarov.Boris@gmail.com>
2024-06-13 09:46:12 +0300

    Update configuration.py

--- Restaurant Subscriber Streaming Service/src/scripts/configuration.py
+++ Restaurant Subscriber Streaming Service/src/scripts/configuration.py
@@ -4,15 +4,15 @@ import os
 KAFKA_BOOTSTRAP_SERVERS = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 'rc1b-2erh7b35n4j4v869.mdb.yandexcloud.net:9091') 
 KAFKA_SECURITY_PROTOCOL = os.environ.get('KAFKA_SECURITY_PROTOCOL', 'SASL_SSL')
 KAFKA_SASL_MECHANISM = os.environ.get('KAFKA_SASL_MECHANISM', 'SCRAM-SHA-512')
-KAFKA_SASL_CONFIG = os.environ.get('KAFKA_SASL_CONFIG',  'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password=\"ltcneltyn\";')
+KAFKA_SASL_CONFIG = os.environ.get('KAFKA_SASL_CONFIG',  'org.apache.kafka.common.security.scram.ScramLoginModule required username="de-student" password=\"*****\";')
 
 #postgres config
 POSTGRES_URL =  os.environ.get('POSTGRES_URL', "jdbc:postgresql://localhost:5432/de")
 POSTGRES_DRIVER = 'org.postgresql.Driver'
 POSTGRES_USER_CLOUD =  os.environ.get('POSTGRES_USER_CLOUD', "student")
-POSTGRES_PASSWORD_CLOUD = os.environ.get('POSTGRES_PASSWORD_CLOUD',  "de-student")
+POSTGRES_PASSWORD_CLOUD = os.environ.get('POSTGRES_PASSWORD_CLOUD',  "*****")
 POSTGRES_USER_LOCAL =  os.environ.get('POSTGRES_USER_LOCAL', "jovyan")
-POSTGRES_PASSWORD_LOCAL = os.environ.get('POSTGRES_PASSWORD_LOCAL',  "jovyan")
+POSTGRES_PASSWORD_LOCAL = os.environ.get('POSTGRES_PASSWORD_LOCAL',  "*****")
 
 #topic names
 TOPIC_NAME_IN = 'kafka.kotlyarovb_in'
